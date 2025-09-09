7972be5131705f9d9e566326d8d6997fa86cf719
rommel dalisay <rommelmdalisay@gmail.com>
2025-09-09 14:17:54 +0800

    one and update product

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -50,3 +50,17 @@ console.log(req.files)
 		product
 	})
 }
+
+exports.getSingleProduct = async (req, res, next) => {
+	const product = await Product.findById(req.params.id);
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not found'
+		})
+	}
+	return res.status(200).json({
+		success: true,
+		product
+	})
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const upload = require("../utils/multer");
 
 const { 
-    newProduct,
+    newProduct, getSingleProduct
     } = require('../controllers/product');
 
 router.post('/admin/product/new', upload.array('images', 10), newProduct);
-
+router.get('/product/:id', getSingleProduct)
 module.exports = router
