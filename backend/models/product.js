0a8f1eca0c5934067beb1bcc1628bb251f354140
rommel dalisay <rommelmdalisay@gmail.com>
2025-09-09 13:40:11 +0800

    add product

--- backend/models/product.js
+++ backend/models/product.js
@@ -89,11 +89,11 @@ const productSchema = new mongoose.Schema({
             }
         }
     ],
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true
-    },
+    // user: {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: 'User',
+    //     required: true
+    // },
     createdAt: {
         type: Date,
         default: Date.now
@@ -101,3 +101,4 @@ const productSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('Product', productSchema);
+
