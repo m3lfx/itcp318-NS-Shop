53c1d9b546935171cfca6bdaa63403db682c5b32
rommel dalisay <rommelmdalisay@gmail.com>
2025-11-04 15:24:43 +0800

    bar chart

--- frontend/src/Components/Admin/Dashboard.jsx
+++ frontend/src/Components/Admin/Dashboard.jsx
@@ -9,6 +9,7 @@ import axios from 'axios'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ProductSalesChart from './ProductSalesChart';
+import UserSalesChart from './UserSalesChart';
 
 
 const Dashboard = () => {
@@ -145,6 +146,9 @@ const Dashboard = () => {
             </div>
             <>
                 <ProductSalesChart />
+            </>
+            <>
+                <UserSalesChart />
             </>
         </ >
     )
